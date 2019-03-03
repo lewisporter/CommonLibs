d5d8cf9b92781b8875521f4f4abf96a95b8f872f
Lewis Porter <lewis.porter@gmail.com>
2019-03-03 23:29:13 +0100

    add implementation of pthread_mutex_timedlock

--- Threads.h
+++ Threads.h
@@ -300,7 +300,34 @@ class Thread {
 
 };
 
+#ifdef __APPLE__
+static int pthread_mutex_timedlock(pthread_mutex_t *mutex, const struct timespec *abs_timeout)
+{
+	int rv;
+	struct timespec remaining, slept, ts;
 
+	remaining = *abs_timeout;
+	while ((rv = pthread_mutex_trylock(mutex)) == EBUSY) {
+		ts.tv_sec = 0;
+		ts.tv_nsec = (remaining.tv_sec > 0 ? 10000000 :
+					  (remaining.tv_nsec < 10000000 ? remaining.tv_nsec : 10000000));
+		nanosleep(&ts, &slept);
+		ts.tv_nsec -= slept.tv_nsec;
+		if (ts.tv_nsec <= remaining.tv_nsec) {
+			remaining.tv_nsec -= ts.tv_nsec;
+		}
+		else {
+			remaining.tv_sec--;
+			remaining.tv_nsec = (1000000 - (ts.tv_nsec - remaining.tv_nsec));
+		}
+		if (remaining.tv_sec < 0 || (!remaining.tv_sec && remaining.tv_nsec <= 0)) {
+			return ETIMEDOUT;
+		}
+	}
+
+	return rv;
+}
+#endif
 
 
 #endif
